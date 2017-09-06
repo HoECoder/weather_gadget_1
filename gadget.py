76acb0f024dc593d06b96b6a96a4ece33a34ac7a
HoECoder <jphansen@gmail.com>
2017-09-05 20:35:50 -0500

    Move to a class setup.

--- gadget.py
+++ gadget.py
@@ -5,26 +5,11 @@ import dht
 import time
 from micropython import const
 
-
-
 temp_form='Temp: {:9.1f}F'
 max_form='Max Temp: {:5.1f}F'
-max_place_holder='Max Temp:   None'
 min_form='Min Temp: {:5.1f}F'
-min_place_holder='Min Temp:   None'
-max_temp=None
-min_temp=None
 humdity_form='Humidity: {:5.0f}%'
-p=None
 
-d1=None
-d2=None
-d5=None
-i2c=None
-oled=None
-sensor=None
-ip_address=Noneeeee
-run_ap=False
 smooth_factor=const(4)
 
 def CtoF(c):
@@ -34,10 +19,9 @@ def smoothing_func(previous,current):
     return (current+smooth_factor*previous)/(smooth_factor+1)
 
 class Gadget:
-    def __init__(self,i2c, sensor, width = 128, height = 64):
+    def __init__(self,i2c, sensor, width=128, height=64):
         self.previous_c = None
         self.current_c = None
-        self.current_f = None
         self.width = width
         self.height = height
         self.i2c = i2c
@@ -54,69 +38,35 @@ class Gadget:
         
     def measure(self):
         self.sensor.measure()
-    
-
-
-def init(do_ap=False):
-    global d1,d2,d5,i2c,oled,sensor,ip_address,run_ap
-    d1=Pin(5) # SCL
-    d2=Pin(4) # SDA
-    d5=Pin(14, Pin.IN) # DHT Sense
-    i2c=I2C(scl=d1, sda=d2)
-    oled=SSD1306_I2C(128, 64, i2c)
-    sensor=dht.DHT22(d5)
-
-    oled.fill(0)
-    oled.text("Loading...", 0, 30)
-    oled.show()
-
-    run_ap=do_ap
-    if do_ap:
-        import network
-        import ubinascii
-        ap_if=network.WLAN(network.AP_IF)
-        essid=b"EnvPy-%s" % ubinascii.hexlify(ap_if.config("mac")[-3:])
-        ap_if.active(True)
-        ap_if.config(essid=essid, authmode=network.AUTH_WPA_WPA2_PSK, password=b"<HAHAHAHAHA>")
-
-        ip_address=b"AP: "+ ap_if.ifconfig()[0]
-
-        print("AP Configured")
-
-def report_temp(display, sense):
-    global max_temp, min_temp, ip_address,run_ap,p
-    sense.measure()
-    c=sense.temperature()
-    if p is None:
+        
+    def report(self):
+        c=sense.temperature()
+        p = self.previous_c
+        if p is None:
+            p=c
+        c=smoothing_func(p,c)
         p=c
-    c=smoothing_func(p,c)
-    p=c
-    hum=sense.humidity()
-    f=CtoF(c)
-    display.fill(0)
-    display.text(temp_form.format(f), 0, 0)
-    if max_temp is None:
-        display.text(max_place_holder, 0, 10)
-        max_temp=f
-    else:
-        display.text(max_form.format(max_temp), 0, 10)
-        max_temp=max(f, max_temp)
+        self.current_c=c
+        self.previous_c=p
+        hum=sense.humidity()
+        f=CtoF(c)
+        self.oled.fill(0)
+        self.oled.text(temp_form.format(f), 0, 0)
+        if self.max_temp is None:
+            self.oled.text(max_form.format(f), 0, 10)
+            self.max_temp=f
+        else:
+            self.oled.text(max_form.format(self.max_temp), 0, 10)
+            self.max_temp=max(f, self.max_temp)
+            
+        if min_temp is None:
+            self.oled.text(min_form.format(f), 0, 20)
+            self.min_temp=f
+        else:
+            self.oled.text(min_form.format(self.min_temp), 0, 20)
+            self.min_temp=min(f, self.min_temp)
+            
+        self.oled.text(humdity_form.format(hum), 0, 30)
         
-    if min_temp is None:
-        display.text(min_place_holder, 0, 20)
-        min_temp=f
-    else:
-        display.text(min_form.format(min_temp), 0, 20)
-        min_temp=min(f, min_temp)
-        
-    display.text(humdity_form.format(hum), 0, 30)
-    
-    if run_ap:
-        display.text(ip_address, 0 , 50)
-    
-    display.show()
-    
-def run():
-    while True:
-        report_temp(oled, sensor)
-        time.sleep(15)
+        self.oled.show()
+
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import gadget
 
-gadget.init()
-gadget.run()
+from machine import Pin, I2C
+import dht
+import time
+
+d1=Pin(5) # SCL
+d2=Pin(4) # SDA
+d5=Pin(14, Pin.IN) # DHT Sense
+i2c=I2C(scl=d1, sda=d2)
+sensor = dht.DHT11(d5)
+
+g = gadget.Gadget(i2c,sensor)
+g.init()
+
+while True:
+    g.measure()
+    g.report()
+    time.sleep(15)
+    
